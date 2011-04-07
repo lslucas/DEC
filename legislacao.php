669efd418a5822e4d2ee7e9e1f85caeebda939dd
Lucas Serafim <lslucas@gmail.com>
2011-04-07 09:00:26 -0500

    convertido para php

---
+++ legislacao.php
@@ -0,0 +1,49 @@
+<?php
+      include_once '_inc.header.php';
+?>
+<h1>LEGISLAÇÃO</h1></td>
+          </tr>
+          <tr>
+            <td><h4>A área da educação tem como base legal, legislação federal, estadual e municipal. Acompanhe a consulte o link de seu interesse.</h4></td>
+          </tr>
+          <tr>
+            <td><table style="background:url(images/banner.png) no-repeat" width="700" border="0" cellspacing="0" cellpadding="0">
+              <tr>
+                <td height="184" align="center" valign="middle"><img src="images/banner_legislacao.png" width="691" height="174" /></td>
+              </tr>
+              <tr>
+                <td height="28" style="height:28;" align="right" valign="middle" ><h5>Fique por dentro das legislações.</h5></td>
+              </tr>
+            </table></td>
+          </tr>
+        </table></td>
+      </tr>
+      <tr>
+        <td height="205" valign="top"><table width="700" border="0" cellspacing="0" cellpadding="0">
+          <tr>
+            <td><h2>UNIDADES ESCOLARES</h2></td>
+          </tr>
+          <tr>
+            <td><table width="665" border="0" cellspacing="0" cellpadding="0" id="botoes">
+              <tr>
+                <td height="54" align="left" valign="middle" ><table width="665" border="0" cellspacing="0" cellpadding="0">
+                  <tr>
+                    <td height="54" width="99" align="left" valign="middle" ><img src="images/btn_federal.png" width="129" height="44" border="0" /></td>
+                    <td width="7">&nbsp;</td>
+                    <td width="99" align="center" valign="middle" ><img src="images/btn_estadual.png" width="129" height="44" border="0" /></td>
+                    <td width="7">&nbsp;</td>
+                    <td width="99"  align="center" valign="middle" ><img src="images/btn_municipal.png" width="129" height="44" border="0" /></td>
+                    <td width="7">&nbsp;</td>
+                    <td  align="center" valign="middle" >&nbsp;</td>
+                    </tr>
+                </table></td>
+              </tr>
+              <tr>
+                <td></td>
+              </tr>
+              </table>
+<?php
+
+    include_once '_inc.footer.php';
+
+?>
